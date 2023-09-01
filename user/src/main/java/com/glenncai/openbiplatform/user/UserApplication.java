a43d8256a5f8f3880e03e50ad72aaf4d8258ce79
Glenn Cai <glenn.lh.cai@gmail.com>
2023-09-02 01:11:49 +0800

    feat: add log aspect, print the log message

--- user/src/main/java/com/glenncai/openbiplatform/user/UserApplication.java
+++ user/src/main/java/com/glenncai/openbiplatform/user/UserApplication.java
@@ -1,7 +1,6 @@
 package com.glenncai.openbiplatform.user;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -9,17 +8,16 @@ import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 @ComponentScan("com.glenncai.openbiplatform")
+@Slf4j
 public class UserApplication {
-
-  private static final Logger LOGGER = LoggerFactory.getLogger(UserApplication.class);
 
   public static void main(String[] args) {
     SpringApplication application = new SpringApplication(UserApplication.class);
     Environment environment = application.run(args).getEnvironment();
     String serverPort = environment.getProperty("server.port");
     String contextPath = environment.getProperty("server.servlet.context-path");
-    LOGGER.info("Running successfully!");
-    LOGGER.info("Access URLs: http://127.0.0.1:{}{}", serverPort, contextPath);
+    log.info("Running successfully!");
+    log.info("Access URLs: http://127.0.0.1:{}{}", serverPort, contextPath);
   }
 
 }
---
+++ user/src/main/java/com/glenncai/openbiplatform/user/aspect/LogAspect.java
@@ -0,0 +1,99 @@
+package com.glenncai.openbiplatform.user.aspect;
+
+import cn.hutool.core.util.RandomUtil;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.support.spring.PropertyPreFilters;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.Signature;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.MDC;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Log aspect to log request info
+ *
+ * @author Glenn Cai
+ * @version 1.0 1/9/2023
+ */
+@Aspect
+@Component
+@Slf4j
+public class LogAspect {
+  public LogAspect() {
+    log.info("Starting log aspect \uD83D\uDE80\uD83D\uDE80\uD83D\uDE80\uD83D\uDE80\uD83D\uDE80");
+  }
+
+  @Pointcut("execution(* com.glenncai.openbiplatform.*.controller.*.*(..))")
+  public void controllerPointcut() {
+  }
+
+  @Before("controllerPointcut()")
+  public void doBefore(JoinPoint joinPoint) {
+
+    // Generate unique request id
+    MDC.put("LOG_ID", System.currentTimeMillis() + RandomUtil.randomString(3));
+
+    // Get request info
+    RequestAttributes requestAttributes = RequestContextHolder.currentRequestAttributes();
+    HttpServletRequest request = ((ServletRequestAttributes) requestAttributes).getRequest();
+    Signature signature = joinPoint.getSignature();
+    String name = signature.getName();
+
+    // Log request info
+    log.info("------------- Start -------------");
+    log.info("Request URL: {} {}", request.getRequestURL().toString(), request.getMethod());
+    log.info("Class Method: {}.{}", signature.getDeclaringTypeName(), name);
+    log.info("Remote Address: {}", request.getRemoteAddr());
+
+    // Log request parameters
+    Object[] args = joinPoint.getArgs();
+
+    // Exclude special parameters, such as file
+    Object[] arguments = new Object[args.length];
+    for (int i = 0; i < args.length; i++) {
+      if (args[i] instanceof ServletRequest
+          || args[i] instanceof ServletResponse
+          || args[i] instanceof MultipartFile) {
+        continue;
+      }
+      arguments[i] = args[i];
+    }
+
+    // Exclude sensitive parameters, such as password
+    String[] excludeProperties = {"password", "confirmPassword"};
+    PropertyPreFilters filters = new PropertyPreFilters();
+    PropertyPreFilters.MySimplePropertyPreFilter excludeFilter = filters.addFilter();
+    excludeFilter.addExcludes(excludeProperties);
+    log.info("Request Parameters: {}", JSON.toJSONString(arguments, excludeFilter));
+  }
+
+  @Around("controllerPointcut()")
+  public Object doAround(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+    long startTime = System.currentTimeMillis();
+    Object result = proceedingJoinPoint.proceed();
+
+    // Exclude sensitive parameters, such as password
+    String[] excludeProperties = {"password", "confirmPassword"};
+    PropertyPreFilters filters = new PropertyPreFilters();
+    PropertyPreFilters.MySimplePropertyPreFilter excludeFilter = filters.addFilter();
+    excludeFilter.addExcludes(excludeProperties);
+
+    log.info("Result: {}", JSON.toJSONString(result, excludeFilter));
+    log.info("------------- End  Time consuming \uD83D\uDD51：{} ms -------------",
+             System.currentTimeMillis() - startTime);
+    return result;
+  }
+}
