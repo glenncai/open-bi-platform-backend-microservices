671410003b6a4cf12af9ffc7660f67d68282a750
Glenn Cai <glenn.lh.cai@gmail.com>
2023-09-01 23:05:16 +0800

    feat: add log setting to user module

--- user/src/main/java/com/glenncai/openbiplatform/user/UserApplication.java
+++ user/src/main/java/com/glenncai/openbiplatform/user/UserApplication.java
@@ -1,15 +1,25 @@
 package com.glenncai.openbiplatform.user;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 @ComponentScan("com.glenncai.openbiplatform")
 public class UserApplication {
 
+  private static final Logger LOGGER = LoggerFactory.getLogger(UserApplication.class);
+
   public static void main(String[] args) {
-    SpringApplication.run(UserApplication.class, args);
+    SpringApplication application = new SpringApplication(UserApplication.class);
+    Environment environment = application.run(args).getEnvironment();
+    String serverPort = environment.getProperty("server.port");
+    String contextPath = environment.getProperty("server.servlet.context-path");
+    LOGGER.info("Running successfully!");
+    LOGGER.info("Access URLs: http://127.0.0.1:{}{}", serverPort, contextPath);
   }
 
 }
