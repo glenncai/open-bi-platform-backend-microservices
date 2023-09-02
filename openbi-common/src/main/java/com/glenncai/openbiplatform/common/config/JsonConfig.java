aeb79d1e7dca0d2a5a387f06e5d2ed7fa5a53e5e
Glenn Cai <glenn.lh.cai@gmail.com>
2023-09-03 00:41:16 +0800

    feat: json config

---
+++ openbi-common/src/main/java/com/glenncai/openbiplatform/common/config/JsonConfig.java
@@ -0,0 +1,34 @@
+package com.glenncai.openbiplatform.common.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+import org.springframework.boot.jackson.JsonComponent;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+
+/**
+ * Spring MVC JSON config
+ *
+ * @author Glenn Cai
+ * @version 1.0 07/26/2023
+ */
+@JsonComponent
+public class JsonConfig {
+
+  /**
+   * Configuration to reduce precision loss when converting Long to Json
+   *
+   * @param builder jackson 2 object mapper builder
+   * @return object mapper
+   */
+  @Bean
+  public ObjectMapper jacksonObjectMapper(Jackson2ObjectMapperBuilder builder) {
+    ObjectMapper objectMapper = builder.createXmlMapper(false).build();
+    SimpleModule simpleModule = new SimpleModule();
+    simpleModule.addSerializer(Long.class, ToStringSerializer.instance);
+    simpleModule.addSerializer(Long.TYPE, ToStringSerializer.instance);
+    objectMapper.registerModule(simpleModule);
+    return objectMapper;
+  }
+}
