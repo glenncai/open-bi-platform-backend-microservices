5fd289f9a32efbcf22430436ec29a89320c55801
Glenn Cai <glenn.lh.cai@gmail.com>
2023-09-04 00:16:59 +0800

    feat: add user constant to user module

---
+++ openbi-user/src/main/java/com/glenncai/openbiplatform/user/constant/UserConstant.java
@@ -0,0 +1,38 @@
+package com.glenncai.openbiplatform.user.constant;
+
+/**
+ * User constant
+ *
+ * @author Glenn Cai
+ * @version 1.0 19/07/2023
+ */
+public final class UserConstant {
+
+  /**
+   * Encryption salt
+   */
+  public static final String SALT = "w_yTivHidq5T";
+
+  /**
+   * Session map key for login user
+   */
+  public static final String LOGIN_USER_STAGE = "openbi:user:stage";
+
+  /**
+   * Default role
+   */
+  public static final String DEFAULT_ROLE = "user";
+
+  /**
+   * Admin role
+   */
+  public static final String ADMIN_ROLE = "admin";
+
+  /**
+   * Ban role
+   */
+  public static final String BAN_ROLE = "ban";
+
+  private UserConstant() {
+  }
+}
