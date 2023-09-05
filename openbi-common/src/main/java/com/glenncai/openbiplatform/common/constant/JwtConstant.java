682fcba05e487717baeb7de472821d784ee17635
Glenn Cai <glenn.lh.cai@gmail.com>
2023-09-05 09:16:04 +0800

    feat: JWT utils

--- openbi-common/src/main/java/com/glenncai/openbiplatform/common/constant/JwtConstant.java
+++ openbi-common/src/main/java/com/glenncai/openbiplatform/common/constant/JwtConstant.java
@@ -1,7 +1,18 @@
-package com.glenncai.openbiplatform.common.constant;/**
-  * This class is for xxx.
-  *
-  * @author Glenn Cai
-  * @version 1.0 5/9/2023
-  */public class JwtConstant {
+package com.glenncai.openbiplatform.common.constant;
+
+/**
+ * Jwt constant
+ *
+ * @author Glenn Cai
+ * @version 1.0 05/09/2023
+ */
+public class JwtConstant {
+
+  /**
+   * Jwt key
+   */
+  public static final String key = "L4llfEd4rY5wuRzOetEi2Keu6Hiahmi8";
+
+  private JwtConstant() {
+  }
 }
