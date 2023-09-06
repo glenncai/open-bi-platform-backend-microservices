a031c3b3d5779759fc2a451089bfe662010617c3
Glenn Cai <glenn.lh.cai@gmail.com>
2023-09-06 15:28:06 +0800

    feat: AI manager tool

---
+++ openbi-ai-analytics/src/main/java/com/glenncai/openbiplatform/aianalytics/mapper/AiManager.java
@@ -0,0 +1,49 @@
+package com.glenncai.openbiplatform.aianalytics.mapper;
+
+import cn.hutool.core.lang.TypeReference;
+import cn.hutool.json.JSONUtil;
+import com.glenncai.openbiplatform.aianalytics.model.dto.ChatRequest;
+import com.glenncai.openbiplatform.aianalytics.model.dto.ChatResponse;
+import com.glenncai.openbiplatform.common.common.BaseResponse;
+import com.glenncai.openbiplatform.common.constant.AiConstant;
+import com.glenncai.openbiplatform.common.exception.BusinessException;
+import com.glenncai.openbiplatform.common.exception.enums.AiExceptionEnum;
+import com.glenncai.openbiplatform.common.utils.HttpUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * Communicate with AI service (e.g. OpenAI, Google Bard, POE, etc.)
+ *
+ * @author Glenn Cai
+ * @version 1.0 07/26/2023
+ */
+@Service
+@Slf4j
+public class AiManager {
+
+  /**
+   * Process AI chat
+   *
+   * @param chatRequest chat request body, including 'message' and 'key'
+   * @return AI response
+   */
+  public String doAiChat(ChatRequest chatRequest) {
+    String jsonStr = JSONUtil.toJsonStr(chatRequest);
+    String result = HttpUtils.post(AiConstant.AI_API_URL, jsonStr);
+    TypeReference<BaseResponse<ChatResponse>> typeRef = new TypeReference<>() {
+    };
+
+    BaseResponse<ChatResponse> response = JSONUtil.toBean(result, typeRef, false);
+    if (response == null) {
+      throw new BusinessException(AiExceptionEnum.AI_RESPONSE_ERROR.getCode(),
+                                  AiExceptionEnum.AI_RESPONSE_ERROR.getMessage());
+    }
+    if (response.getCode() != 0) {
+      throw new BusinessException(AiExceptionEnum.AI_RESPONSE_ERROR.getCode(),
+                                  AiExceptionEnum.AI_RESPONSE_ERROR.getMessage());
+    }
+
+    return response.getData().getContent();
+  }
+}
